d6a8386f096f5b61b2e2bd97e30f4c953d671f60
emrebdr <emrebdr29@gmail.com>
2023-02-22 23:19:52 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # logger
+
+'logger' is a simple logging library for Go.
+
+## Installation
+
+    go get github.com/emrebdr/logger
+
+## Usage
+``` go
+import(
+    "github.com/emrebdr/logger"
+)
+
+logger.Log("this is a debug message")
+logger.LogInfo("this is an info message")
+logger.LogWarning("this is a warning message")
+logger.LogError("this is an error message")
+logger.LogSuccess("this is a success message")
+```
+
+## License
+
+MIT
---
+++ logger.go
@@ -0,0 +1,49 @@
+package logger
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/fatih/color"
+)
+
+var WHITE = color.New(color.FgWhite).SprintFunc()
+var RED = color.New(color.FgRed).SprintFunc()
+var GREEN = color.New(color.FgGreen).SprintFunc()
+var YELLOW = color.New(color.FgYellow).SprintFunc()
+var BLUE = color.New(color.FgBlue).SprintFunc()
+
+func Log(v ...interface{}) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	str := fmt.Sprintf("%s [LOG] ", currentTime)
+	fmt.Printf(WHITE(str) + " ")
+	fmt.Println(v...)
+}
+
+func LogError(v ...interface{}) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	str := fmt.Sprintf("%s [ERROR] ", currentTime)
+	fmt.Printf(RED(str) + " ")
+	fmt.Println(v...)
+}
+
+func LogSuccess(v ...interface{}) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	str := fmt.Sprintf("%s [SUCCESS] ", currentTime)
+	fmt.Printf(GREEN(str) + " ")
+	fmt.Println(v...)
+}
+
+func LogWarning(v ...interface{}) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	str := fmt.Sprintf("%s [WARNING] ", currentTime)
+	fmt.Printf(YELLOW(str) + " ")
+	fmt.Println(v...)
+}
+
+func LogInfo(v ...interface{}) {
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	str := fmt.Sprintf("%s [INFO] ", currentTime)
+	fmt.Printf(BLUE(str) + " ")
+	fmt.Println(v...)
+}
